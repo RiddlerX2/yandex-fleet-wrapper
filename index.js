1a6c084207041732016aa200d50faf2c46a98bed
RiddlerX2 <timofeev.andreyx2@gmail.com>
2021-09-12 22:26:46 +0300

    1.0.1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /*
-	IQSMS wrapper v.1.0.x
-	Allow send SMS using IQSMS service
+	Yandex.Fleet wrapper v.1.0.x
+	Allow send commands to Yandex services
+	This module for car rent services only! Not for taxi drivers or customers!
 	
 	For more information about operations and its parameters look at:
-		https://iqsms.ru/api/api_rest/
+		https://fleet.taxi.yandex.ru/docs/api/reference/index.html
 */
 
 /*Define dependencies*/
@@ -13,7 +14,7 @@ const axios = require('axios');
 const messages = {
 	ru : {
 		operation_error : 'Указанная операция не определена в документации',
-		params_invalid : 'Параметры должны быть переданы в виде массива',
+		params_invalid : 'Параметры должны быть переданы в виде объекта',
 		callback_invalid : 'Требуется функция обратного вызова',
 		value_invalid : 'Параметр должен быть указан',
 		value_type_invalid : 'Некорректный тип данных ',
@@ -21,7 +22,7 @@ const messages = {
 	},
 	en : {
 		operation_error : 'The specified operation is not defined in the documentation',
-		params_invalid : 'Parameters must be passed as an array',
+		params_invalid : 'Parameters must be passed as an object',
 		callback_invalid : 'Callback function required',
 		value_invalid : 'Value must be specified',
 		value_type_invalid : 'Value type is incorrect ',
@@ -30,34 +31,36 @@ const messages = {
 }
 
 const commands = [
-	'balance', 
-	'send',
-	'status'
+	'cars/list', 
+	'driver-profiles/list',
+	'orders/list',
+	'orders/track',
+	'driver-profiles/transactions',
+	'driver-profiles/transactions/list',
+	'transactions/list',
+	'orders/transactions/list'
 ];
 
 /*Main class*/
-class IQSMS {
+class Fleet {
+	queueList = [];
 	#language;
-	#username;
-	#password;
-	#signature;
-	/*Default REST point of IQSMS*/
-	execURLPrefix = `https://api.iqsms.ru/messages/v2/`;
-	execURLSuffix = `.json`;
+	#parkID;
+	#token;
+	#timerID = null;
+	/*Default REST point of Yandex.Fleet*/
+	execURLPrefix = `https://fleet-api.taxi.yandex.net/v1/parks/`;
+	execURLSuffix = ``;
 	/*Initialise class with "new"*/
-	constructor (userName, password, language, signature, execURLPrefix, execURLSuffix) {
+	constructor (parkID, token, language, execURLPrefix, execURLSuffix) {
 		this.#language = language;
 
-		if (!userName || !password) {
+		if (!parkID || !token) {
 			throw messages[this.#language].auth_invalid;
 		}
 
-		this.#username = userName;
-		this.#password = password;
-
-		if (signature) {
-			this.#signature = signature;
-		}
+		this.#parkID = parkID;
+		this.#token = token;
 
 		/*If URLs not defines use default values*/
 		if (execURLPrefix) {
@@ -76,39 +79,50 @@ class IQSMS {
 	isArray = (a) => {
     return (!!a) && (a.constructor === Array);
 	};
+
+	randHex = (len) => {
+		let maxlen = 8,
+			min = Math.pow(16, Math.min(len, maxlen) - 1) 
+			max = Math.pow(16, Math.min(len, maxlen)) - 1,
+			n   = Math.floor(Math.random() * (max - min + 1)) + min,
+			r   = n.toString(16);
+		while (r.length < len) {
+			r += randHex(len - maxlen);
+		}
+		return r;
+	};	
 	
 	/*Unified execute action with callback*/
 	execute(operation, data, callback) {
-		let sdata = {};
 		if (!callback instanceof Function) {
 			throw messages[this.#language].callback_invalid;
-		} else if (!this.isArray(data)) {
+		} else if (!this.isObject(data)) {
 			callback(messages[this.#language].params_invalid, false);
 		} else if (!commands.includes(operation)) {
 			callback(messages[this.#language].operation_error, false);
 		} else {
-			/*Extend parameters by adding authorization token*/
-			if (this.#username && this.#password) {
-				sdata.login = this.#username;
-				sdata.password = this.#password;
-			}
-			if (data.length) {
-				sdata.messages = data;
-			}
+			// Prepare here!!!
+
 			/*Send data to server*/
 			axios({
 				method : 'POST', //as described in documentation (link above)
 				url : `${this.execURLPrefix}${operation}${this.execURLSuffix}`,
-				data : sdata
+				headers : {
+					'X-Client-ID' : `taxi/park/${this.#parkID}`,
+					'X-API-Key' : this.#token,
+					'X-Idempotency-Token' : this.randHex(32)
+				},
+				data : data
 			}).then(
 				(result) => {
+					console.log(result);
 					/*If operation succeeded on API's side return data*/
-					if (result.data.status == 'ok') {
-						callback(false, result.data);
-					} else {
+					//if (result.data.status == 'ok') {
+					//	callback(false, result.data);
+					//} else {
 						/*Else return data with description as error*/
-						callback(result.data, false);
-					}
+					//	callback(result.data, false);
+					//}
 				}
 			).catch(
 				(error) => {
@@ -117,10 +131,10 @@ class IQSMS {
 			)
 		}
 	}
-	/*Promisified execution for those who used a single thread application and uses await*/
+
 	executePromise(operation, data) {
 		return new Promise((resolve, reject) => {
-			this.execute(operation, data, (error, data) => {
+			this.execute(operation, params, (error, data) => {
 				if (error) {
 					reject(error);
 				} else {
@@ -130,28 +144,36 @@ class IQSMS {
 		});
 	}
 
-	/*Functions for most popular actions*/
-	send(phone, message) {
-		let cphone = phone.replace(/[ ,\(,\),\-]/g, '');
-		let messages = [{
-			phone : cphone,
-			clientId : Math.round(Math.random() * 1000000),
-			text : message
-		}];
-		if (this.#signature) {
-			messages[0].sender = this.#signature;
+	async runQueue() {
+		let item = this.queueList[0];
+		if (item) {
+			this.queueList.splice(0, 1);
+			try {
+				let data = await this.executePromise(item.operation, item.data);
+				item.callback(false, data);
+			} catch (error) {
+				item.callback(data, false);
+			}	
 		}
-		return this.executePromise('send', messages);
-	}
-	
-	status(messages) {
-		return this.executePromise('status', messages);
+		if (this.queueList.length > 0) {
+			this.#queueID = setTimeout(() => {this.runQueue()}, 1000);
+		} else {
+			this.#timerID = null
+		}
 	}
 
-	balance() {
-		return this.executePromise('balance', []);
+	queue(operation, data, callback) {
+		let item = {
+			operation : operation,
+			data : data,
+			callback : callback
+		}
+		this.queueList.push(item);
+		if (!this.#timerID) {
+			this.#queueID = setTimeout(() => {this.runQueue()}, 0);
+		}
 	}
 }
 
 /*Export class to outside*/
-exports.IQSMS = IQSMS;
+exports.Fleet = Fleet;
