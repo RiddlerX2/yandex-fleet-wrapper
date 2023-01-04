1c5a63f6f596539b5b743847d5bb8ca3d824a4f9
RiddlerX2 <timofeev.andreyx2@gmail.com>
2023-01-04 14:30:43 +0300

    1.2.1

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # yandex-fleet-wrapper
- Wrapper for Yandex.Fleet API
+Wrapper for Yandex.Fleet API
+
+- Allow send commands to Yandex services
+- This module for car rent services only! Not for taxi drivers or customers!
+
+For more information about operations and its parameters look at:
+    https://fleet.taxi.yandex.ru/docs/api/reference/index.html
+
+Обертка над сервисами Яндекс.Такси
+Позволяет посылать команды в сервис
+Данный модуль предназначен только для таксопарков и сервисов аренды авто.
+Модуль не предназначен для водителей и клиентов такси.
+
+За дополнительной информацией по API обращайтесь к документации по адресу:
+    https://fleet.taxi.yandex.ru/docs/api/reference/index.html
+
+Installation:
+    npm i yandex-fleet-wrapper
+
+Usage:
+Create object
+```
+const Fleet = require('yandex-fleet-wrapper').Fleet;
+
+let fleet = new Fleet(
+    'Yuor park ID', 
+    'Yuor park auth token'
+    [, 'Language code (ru, en)' 
+    [, 'URL Prefix' 
+    [, 'URL suffix' ]]]
+);
+```
+If Language code not provided 'ru' is used by default
+if URL Prefix and Suffix not provided default is used.
+__Yandex services path can be changes without notice, this project can be updated with delay__
+
+All sending commands are queueing and executes sequentially with delay of 2 seconds. This is because Yandex.API requires a minimum delay of 0.5sec between operations on same parkId.
+
+Get drivers list:
+```
+let data = await fleet.drivers();
+```
+
+Get orders list:
+```
+let data = await fleet.orders(timeFrom, timeTo);
+```
+timeFrom and timeTo is Date object
+timeFrom <= timeTo
+
+Get transactions list:
+```
+let data = await fleet.transactions(friverId, timeFrom, timeTo);
+```
+driverId is an Yandex ID that you can get using fleet.drivers() method
+timeFrom and timeTo is Date object
+timeFrom <= timeTo
+
+Make driver transaction:
+```
+let data = await fleet.transaction(driverId, moneyAmount, remarks, idKey);
+```
+driverId is an Yandex ID that you can get using fleet.drivers() method
+moneyAmount should be a positive or negative decimal(12,2) value and can't be zero
+remarks could describe your transaction and passed "AS IS" to Yandex transaction log
+idKey (idempotency key) is an unique ID of the transaction provided by user
+    If one transaction with the same idKey is executed then next one is declined
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 /*
-	Yandex.Fleet wrapper v.1.0.x
-	Allow send commands to Yandex services
-	This module for car rent services only! Not for taxi drivers or customers!
-	
-	For more information about operations and its parameters look at:
-		https://fleet.taxi.yandex.ru/docs/api/reference/index.html
+    Yandex.Fleet wrapper v.1.2.1
+    Allow send commands to Yandex services
+    This module for car rent services only! Not for taxi drivers or customers!
+    
+    For more information about operations and its parameters look at:
+        https://fleet.taxi.yandex.ru/docs/api/reference/index.html
+
+    Обертка над сервисами Яндекс.Такси
+    Позволяет посылать команды в сервис
+    Данный модуль предназначен только для таксопарков и сервисов аренды авто.
+    Модуль не предназначен для водителей и клиентов такси.
+
+    За дополнительной информацией по API обращайтесь к документации по адресу:
+        https://fleet.taxi.yandex.ru/docs/api/reference/index.html
 */
 
 /*Define dependencies*/
@@ -12,283 +20,309 @@ const axios = require('axios');
 
 /*Error mesages*/
 const messages = {
-	ru : {
-		operation_error : 'Указанная операция не определена в документации',
-		params_invalid : 'Параметры должны быть переданы в виде объекта',
-		callback_invalid : 'Требуется функция обратного вызова',
-		value_invalid : 'Параметр должен быть указан',
-		value_type_invalid : 'Некорректный тип данных ',
-		auth_invalid : 'Некорректные данные для авторизации, необходимы имя пользователя и пароль'
-	},
-	en : {
-		operation_error : 'The specified operation is not defined in the documentation',
-		params_invalid : 'Parameters must be passed as an object',
-		callback_invalid : 'Callback function required',
-		value_invalid : 'Value must be specified',
-		value_type_invalid : 'Value type is incorrect ',
-		auth_invalid : 'Incorrect authorization userName and password must be provided'
-	}
+    ru : {
+        operation_error : 'Указанная операция не определена в документации',
+        params_invalid : 'Параметры должны быть переданы в виде объекта',
+        callback_invalid : 'Требуется функция обратного вызова',
+        value_invalid : 'Параметр должен быть указан',
+        value_type_invalid : 'Некорректный тип данных ',
+        auth_invalid : 'Некорректные данные для авторизации, необходимы имя пользователя и пароль'
+    },
+    en : {
+        operation_error : 'The specified operation is not defined in the documentation',
+        params_invalid : 'Parameters must be passed as an object',
+        callback_invalid : 'Callback function required',
+        value_invalid : 'Value must be specified',
+        value_type_invalid : 'Value type is incorrect ',
+        auth_invalid : 'Incorrect authorization userName and password must be provided'
+    }
 }
 
 const commands = [
-	'v1/parks/cars/list', 
-	'v1/parks/driver-profiles/list',
-	'v1/parks/orders/list',
-	'v1/parks/orders/track',
-	'v2/parks/driver-profiles/transactions',
-	'v2/parks/driver-profiles/transactions/list',
-	'v2/parks/transactions/list',
-	'v2/parks/orders/transactions/list'
+    /*Append on 1.0.0*/
+    'v1/parks/driver-profiles/list', //
+    'v1/parks/orders/list', //
+    'v2/parks/driver-profiles/transactions/list', //
+    'v2/parks/driver-profiles/transactions', //
+    /*Append on 1.2.0*/
+    '/v1/parks/driver-work-rules',
+    /*Planned on 1.2.x*/
+    '/v2/parks/vehicles/car',
+    '/v1/parks/cars/list',
+    '/v1/parks/driver-profiles/car-bindings',
+    '/v2/parks/contractors/driver-profile',
+    'v1/parks/orders/track',
+    'v2/parks/transactions/list',
+    'v2/parks/orders/transactions/list',
 ];
 
 /*Main class*/
 class Fleet {
-	queueList = [];
-	#language;
-	#parkID;
-	#token;
-	#timerID = null;
-	/*Default REST point of Yandex.Fleet*/
-	execURLPrefix = `https://fleet-api.taxi.yandex.net/`;
-	execURLSuffix = ``;
-	/*Initialise class with "new"*/
-	constructor (parkID, token, language, execURLPrefix, execURLSuffix) {
-		this.#language = language;
+    queueList = [];
+    #language;
+    #parkID;
+    #token;
+    /*Default REST point of Yandex.Fleet*/
+    execURLPrefix = `https://fleet-api.taxi.yandex.net/`;
+    execURLSuffix = ``;
+    /*Initialise class with "new"*/
+    constructor (parkID, token, language, execURLPrefix, execURLSuffix) {
+        this.#language = language;
 
-		if (!parkID || !token) {
-			throw messages[this.#language].auth_invalid;
-		}
+        if (!parkID || !token) {
+            throw messages[this.#language].auth_invalid;
+        }
 
-		this.#parkID = parkID;
-		this.#token = token;
+        this.#parkID = parkID;
+        this.#token = token;
 
-		/*If URLs not defines use default values*/
-		if (execURLPrefix) {
-			this.execURLPrefix = execURLPrefix;
-		};
-		if (execURLSuffix) {
-			this.execURLSuffix = execURLSuffix;
-		};
-		this.#timerID = setTimeout(() => {this.runQueue()}, 0);
-	};
-	
-	/*Simple detectors of Objects and Arrays*/
-	isObject = (a) => {
-		return (!!a) && (a.constructor === Object);
-	};
-	
-	isArray = (a) => {
+        /*If URLs not defines use default values*/
+        if (execURLPrefix) {
+            this.execURLPrefix = execURLPrefix;
+        };
+        if (execURLSuffix) {
+            this.execURLSuffix = execURLSuffix;
+        };
+        setTimeout(() => {this.runQueue()}, 0);
+    };
+    
+    /*Simple detectors of Objects and Arrays*/
+    isObject = (a) => {
+        return (!!a) && (a.constructor === Object);
+    };
+    
+    isArray = (a) => {
     return (!!a) && (a.constructor === Array);
-	};
+    };
 
-	randHex = (len) => {
-		let maxlen = 8,
-			min = Math.pow(16, Math.min(len, maxlen) - 1), 
-			max = Math.pow(16, Math.min(len, maxlen)) - 1,
-			n   = Math.floor(Math.random() * (max - min + 1)) + min,
-			r   = n.toString(16);
-		while (r.length < len) {
-			r += this.randHex(len - maxlen);
-		}
-		return r;
-	};	
-	
-	/*Unified execute action with callback*/
-	execute(operation, data, idKey, callback) {
-		if (!callback instanceof Function) {
-			throw messages[this.#language].callback_invalid;
-		} else if (!this.isObject(data)) {
-			callback(messages[this.#language].params_invalid, false);
-		} else if (!commands.includes(operation)) {
-			callback(messages[this.#language].operation_error, false);
-		} else {
-			/*Send data to server*/
-			/*TO DO: Multipaging*/
-			axios({
-				method : 'POST', //as described in documentation (link above)
-				url : `${this.execURLPrefix}${operation}${this.execURLSuffix}`,
-				headers : {
-					'X-Client-ID' : `taxi/park/${this.#parkID}`,
-					'X-API-Key' : this.#token,
-					'X-Idempotency-Token' : idKey || this.randHex(32)
-				},
-				data : data
-			}).then(
-				(result) => {
-					/*If operation succeeded on API's side return data*/
-					callback(false, result.data);
-				}
-			).catch(
-				(error) => {
-					callback(error, false);
-				}
-			)
-		}
-	}
+    randHex = (len) => {
+        let maxlen = 8,
+            min = Math.pow(16, Math.min(len, maxlen) - 1), 
+            max = Math.pow(16, Math.min(len, maxlen)) - 1,
+            n   = Math.floor(Math.random() * (max - min + 1)) + min,
+            r   = n.toString(16);
+        while (r.length < len) {
+            r += this.randHex(len - maxlen);
+        }
+        return r;
+    };	
+    
+    /*Unified execute action with callback*/
+    execute(operation, method, data, idKey, callback) {
+        if (!callback instanceof Function) {
+            throw messages[this.#language].callback_invalid;
+        } else if (!this.isObject(data)) {
+            callback(messages[this.#language].params_invalid, false);
+        } else if (!commands.includes(operation)) {
+            callback(messages[this.#language].operation_error, false);
+        } else {
+            /*Send data to server*/
+            let query = {
+                method : method,
+                url : `${this.execURLPrefix}${operation}${this.execURLSuffix}`,
+                headers : {
+                    'X-Client-ID' : `taxi/park/${this.#parkID}`,
+                    'X-API-Key' : this.#token,
+                    'X-Idempotency-Token' : idKey || this.randHex(32)
+                }
+            };
+            if (method === 'GET') {
+                query.params = data
+            } else {
+                query.data = data
+            }
 
-	/*Promisified execution for queue*/
-	executePromise(operation, data, idKey) {
-		return new Promise((resolve, reject) => {
-			this.execute(operation, data, idKey, (error, data) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(data);
-				}
-			});
-		});
-	}
+            axios(query)
+                .then((result) => {
+                    /*If operation succeeded on API's side return data*/
+                    callback(false, result.data);
+                })
+                .catch((error) => {
+                    /*If operation failed? return error*/
+                    callback(error, false);
+                });
+        }
+    }
 
-	/*Queue runner once per second // Yandex not allow more then 1 query per 0.5 seconds that's why using delayed queue*/
-	async runQueue() {
-		let item = this.queueList[0];
-		if (item) {
-			try {
-				/*Wait until operation finished because of Yandex limitations*/
-				let data = await this.executePromise(item.operation, item.data, item.idKey);
-				item.callback(false, data);
-			} catch (error) {
-				item.callback(error, false);
-			}	
-			this.queueList.splice(0, 1);
-		}
-		this.#timerID = setTimeout(() => {this.runQueue()}, 2000);
-	}
+    /*Promisified execution for queue*/
+    executePromise(operation, method, data, idKey) {
+        return new Promise((resolve, reject) => {
+            this.execute(operation, method, data, idKey, (error, data) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
 
-	/*Append task to queue*/
-	queue(operation, data, idKey, callback) {
-		let item = {
-			operation : operation,
-			data : data,
-			callback : callback,
-			idKey : idKey
-		}
-		this.queueList.push(item);
-	}
+    /*Queue runner once per second // Yandex not allow more then 1 query per 0.5 seconds that's why using delayed queue*/
+    async runQueue() {
+        let item = this.queueList[0];
+        if (item) {
+            try {
+                /*Wait until operation finished because of Yandex limitations*/
+                let data = await this.executePromise(item.operation, item.method, item.data, item.idKey);
+                item.callback(false, data);
+            } catch (error) {
+                item.callback(error, false);
+            }	
+            this.queueList.splice(0, 1);
+        }
+        setTimeout(() => {this.runQueue()}, 2000);
+    }
 
-	/*Simplified task creator for listing all drivers*/
-	drivers() {
-		let data = {
-			limit : 1000,
-			offset : 0,
-			query : {
-				park : {
-					id : this.#parkID
-				},
-				fields : {
-					car : [],
-					park : []
-				}
-			},
-			sort_order : [
-				{
-					direction : 'desc',
-					field : 'driver_profile.created_date'
-				}
-			]
-		};
-		return new Promise((resolve, reject) => {
-			this.queue('v1/parks/driver-profiles/list', data, null, (err, res) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(res);
-			});
-		});
-	}
+    /*Append task to queue*/
+    queue(operation, method, data, idKey, callback) {
+        let item = {
+            method : method,
+            operation : operation,
+            data : data,
+            callback : callback,
+            idKey : idKey
+        }
+        this.queueList.push(item);
+    }
 
-	/*Simplified task creator for listing all orders*/
-	orders(timeFrom, timeTo) {
-		let data = {
-			limit : 500,
-			query : {
-				park : {
-					id : this.#parkID,
-					order : {
-						ended_at : {
-							from : timeFrom,
-							to : timeTo
-						}
-					}
-				}
-			}
-		};
+    /*Task for listing all drivers*/
+    drivers() {
+        let data = {
+            limit : 1000,
+            offset : 0,
+            query : {
+                park : {
+                    id : this.#parkID
+                },
+                fields : {
+                    car : [],
+                    park : []
+                }
+            },
+            sort_order : [
+                {
+                    direction : 'desc',
+                    field : 'driver_profile.created_date'
+                }
+            ]
+        };
+        return new Promise((resolve, reject) => {
+            this.queue(commands[0], 'POST', data, null, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    }
 
-		return new Promise((resolve, reject) => {
-			this.queue('v1/parks/orders/list', data, null, (err, res) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(res);
-			});
-		});
-	}
+    /*Task for listing all orders*/
+    orders(timeFrom, timeTo) {
+        let data = {
+            limit : 500,
+            query : {
+                park : {
+                    id : this.#parkID,
+                    order : {
+                        ended_at : {
+                            from : timeFrom,
+                            to : timeTo
+                        }
+                    }
+                }
+            }
+        };
 
-	/*Simplified task creator for listing all transactions*/
-	transactions(driver, timeFrom, timeTo) {
-		let data = {
-			limit : 1000,
-			query : {
-				park : {
-					id : this.#parkID,
-					driver_profile : {
-						id : driver
-					},
-					transaction : {
-						event_at : {
-							from : timeFrom,
-							to : timeTo
-						}
-					}
-				}
-			}
-		};
+        return new Promise((resolve, reject) => {
+            this.queue(commands[1], 'POST', data, null, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    }
 
-		for (let item of this.queueList) {
-			if (item.data.query.park.driver_profile == data.query.park.driver_profile) {
-				return new Promise((resolve, reject) => {
-					reject('Duplicate queue entry ' + JSON.stringify(data));
-				});
-			}
-		}
+    /*Task for listing all transactions*/
+    transactions(driver, timeFrom, timeTo) {
+        let data = {
+            limit : 1000,
+            query : {
+                park : {
+                    id : this.#parkID,
+                    driver_profile : {
+                        id : driver
+                    },
+                    transaction : {
+                        event_at : {
+                            from : timeFrom,
+                            to : timeTo
+                        }
+                    }
+                }
+            }
+        };
 
-		return new Promise((resolve, reject) => {
-			this.queue('v2/parks/driver-profiles/transactions/list', data, null, (err, res) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(res);
-			});
-		});
-	}
+        for (let item of this.queueList) {
+            if (item.data.query.park.driver_profile == data.query.park.driver_profile) {
+                return new Promise((resolve, reject) => {
+                    reject('Duplicate queue entry ' + JSON.stringify(data));
+                });
+            }
+        }
 
-	/*Simplified task for creating new transaction*/
-	transaction(driver, amount, remarks, idKey) {
-		let data = {
-			amount: `${amount}`,
-			category_id: 'partner_service_manual',
-			description: remarks,
-			driver_profile_id: driver,
-			park_id: this.#parkID
-		};
+        return new Promise((resolve, reject) => {
+            this.queue(commands[2], 'POST', data, null, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    }
 
-		for (let item of this.queueList) {
-			if (item.data.amount == data.amount && item.data.description == data.description && item.data.driver_profile_id == data.driver_profile_id) {
-				return new Promise((resolve, reject) => {
-					reject('Duplicate queue entry ' + JSON.stringify(data));
-				});
-			}
-		}
+    /*Task for creating new transaction*/
+    transaction(driver, amount, remarks, idKey) {
+        let data = {
+            amount: `${amount}`,
+            category_id: 'partner_service_manual',
+            description: remarks,
+            driver_profile_id: driver,
+            park_id: this.#parkID
+        };
 
-		return new Promise((resolve, reject) => {
-			this.queue('v2/parks/driver-profiles/transactions', data, idKey, (err, res) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(res);
-			});
-		});
-	}
+        for (let item of this.queueList) {
+            if (item.data.amount == data.amount && item.data.description == data.description && item.data.driver_profile_id == data.driver_profile_id) {
+                return new Promise((resolve, reject) => {
+                    reject('Duplicate queue entry ' + JSON.stringify(data));
+                });
+            }
+        }
+
+        return new Promise((resolve, reject) => {
+            this.queue(commands[3], 'POST', data, idKey, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    }
+
+    /*Task for listing all workrules*/
+    workRules() {
+        let data = {
+            park_id: this.#parkID
+        }
+        return new Promise((resolve, reject) => {
+            this.queue(commands[4], 'GET', data, idKey, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+    }
 }
 
 /*Export class to outside*/
